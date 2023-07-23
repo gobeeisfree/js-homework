46776d25aba7f21fed8862c029cc484b590aba92
gobeeisfree <gobeeisfree@gmail.com>
2023-07-23 21:02:57 +0900

    2차 과제 완성

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -9,13 +9,51 @@
 
 */
 
+import { data } from "./data.js";
+import { addClass, attr, css, getNode, removeClass } from "./lib/index.js";
 
 
+const navigation = getNode('.nav ul')
 
 
+function setBgColor(target, colorA, colorB="#000") {
+  css(target, 'background', `linear-gradient(to bottom, ${colorA}, ${colorB})`)
+}
 
 
+function setImage(src, alt) {
+  const visualImage = getNode('.visual img')
+  attr(visualImage, 'src', src)
+  attr(visualImage, 'alt', alt)
+}
 
 
+function setNameText(value) {
+  const nickName = getNode('.nickName')
+  nickName.textContent = value;
+}
 
 
+function handleSlider(e) {
+  const body = getNode('body')
+  const target = e.target.closest('li');
+
+  if (!target) return;
+
+  const index = attr(target, 'data-index') - 1;
+
+  const list = [...navigation.children];
+  list.forEach((list)=>{
+    removeClass(list, 'is-active');
+  });
+
+  addClass(target, 'is-active');
+
+
+  setBgColor(body, `${data[index].color[0]}`, `${data[index].color[1]}`)
+  setImage(`./assets/${data[index].name.toLowerCase()}.jpeg`, `${data[index].alt}`)
+  setNameText(data[index].name);
+}
+
+
+navigation.addEventListener('click', handleSlider);
